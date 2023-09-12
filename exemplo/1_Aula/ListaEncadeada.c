e93d4089aa65626661b24c7fe5d61fe7f8ba3d9c
Pedro <pedrosantos121229@gmail.com>
2023-09-12 09:42:31 -0300

    teste

--- exemplo/1_Aula/ListaEncadeada.c
+++ exemplo/1_Aula/ListaEncadeada.c
@@ -21,14 +21,67 @@ void inserir_no_inicio(No **lista, int num)
         *lista = novo;
     }
     else
-        printf("Falha na alocacao de memoria...\n");
-
+        printf("Falha na alocacao de memoria...\n\n");
 }
 
+//aula 244 - procedimento para isnserir no final
+void inserir_no_final(No **lista, int num)
+{
+    No *auxiliar, *novo = malloc(sizeof(No));
+
+    if(novo){
+        novo->valor = num;
+        novo->proximo = NULL;
+
+        // verifica se aLista esta vazia ja q se sestiver o 
+        // elemento vai ser inserido no inicio da lista
+
+        if(*lista == NULL)
+        {
+            *lista = novo;
+        }else
+        {
+            auxiliar = *lista;
+
+            while (auxiliar->proximo)// != NULL // Percorre a lista ate ser NULL o q indica que é o final da lista
+                auxiliar = auxiliar->proximo;
+
+            auxiliar->proximo = novo;
+        }
+        
+        *lista = novo;
+    }else
+        printf("Erro na alocacao de memoria...\n\n");
+}
+
+// aula 245 - rocedimento para inserir no final meio
+void inserir_no_meio(No **lista, int num, int anterior)
+{
+    No *auxiliar, *novo = malloc(sizeof(No));
+    if(novo)
+    {
+        novo->valor = num;
+        novo->proximo = NULL;
+
+        if(*lista == NULL)
+        {
+            novo->proximo = NULL;
+            *lista = novo;
+        }
+           
+
+        auxiliar = *lista;
+
+        while (auxiliar->valor != anterior && auxiliar->proximo)// caso o valor informado ano esxistir o novo sera inserido no final da lista
+            auxiliar = auxiliar->proximo;
+        
+        novo->proximo = auxiliar->proximo;
+        auxiliar->proximo = novo;
+    }
+    else
+        printf("Erro na alocacao de memoria...\n\n");   
+}
 int main(){
-
-
-
 
     return 0;
 }
